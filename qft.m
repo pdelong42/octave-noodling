7ddf40e5b67e6d8a4a92ebc9b1ba5dfc8cd4b0da
Paul DeLong <Paul.DeLong@gmail.com>
2023-01-29 23:37:18 -0500

    first stab at a full-fledged equation;

--- qft.m
+++ qft.m
@@ -8,4 +8,10 @@ pkg load symbolic
 
 #help sym
 #help syms
-help symfun
+#help symfun
+
+syms hbar V A_n B_n E_n p_n t x phi(x)
+
+phi(x) = sqrt( hbar / ( 2 * V * E_n ) ) * ( A_n * exp( - i * ( E_n * t - p_n * x ) / hbar ) + B_n * exp( +i * ( E_n * t - p_n * x ) / hbar ) )
+
+# B_n should be B_n-dagger, because it's the Hermitian conjugate.  I'm looking to see if there's a way to account for this.
